2beeba75e3a632f5983ccd9261f0dc745914ade9
Sameh Mabrouk <smabrouk@mobiquityinc.com>
2017-11-23 13:06:56 +0100

    Soccer Game

---
+++ Games/Soccer/SoccerGame.swift
@@ -0,0 +1,8 @@
+//
+//  SoccerGame.swift
+//  Swift Alps Game Jam
+//
+//  Created by Sameh Mabrouk on 23/11/2017.
+//
+
+import Foundation
---
+++ Games/Soccer/SoccerScene.swift
@@ -0,0 +1,8 @@
+//
+//  SoccerScene.swift
+//  Swift Alps Game Jam
+//
+//  Created by Sameh Mabrouk on 23/11/2017.
+//
+
+import Foundation
