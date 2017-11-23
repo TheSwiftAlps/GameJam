091ac24ade0716eeabc342ef8e653a757c018710
Eran Jalink <jalink@datlinq.com>
2017-11-23 18:29:39 +0100

    reverted Bundle
    This reverts commit c52cc62a65bf52d2a9a1f798046811a7287a4568.

--- SpriteKitPlayground.playground/Contents.swift
+++ SpriteKitPlayground.playground/Contents.swift
@@ -2,167 +2,79 @@ import SpriteKit
 import PlaygroundSupport
 
 struct Categories {
-    static let rocket: UInt32 = 1
-    static let enemy: UInt32 = 1 << 1
+    static let ground: UInt32 = 1
+    static let coins: UInt32 = 1 << 1
 }
 
 class Scene: SKScene {
-    
-    //MARK: Objects
-    lazy var deathStar: SKSpriteNode = {
-        let deathStar = SKSpriteNode(imageNamed: "Character")
-        deathStar.position.x = frame.midX
-        deathStar.position.y = frame.midY
-        
-        return deathStar
-    }()
-    
-    func makeRocket() {
-        let rocket = SKSpriteNode(imageNamed: "Rocket")
-        rocket.position.x = frame.midX
-        rocket.position.y = frame.midY
-        rocket.zRotation = deathStar.zRotation
-        rocket.physicsBody = SKPhysicsBody(rectangleOf: rocket.size)
-        rocket.physicsBody?.categoryBitMask = Categories.rocket
-        rocket.physicsBody?.contactTestBitMask = Categories.enemy
-        rocket.physicsBody?.collisionBitMask = 0
-        addChild(rocket)
-        fireRocket(rocket)
-    }
-    
-    func makeEnemy() {
-        let enemy = SKSpriteNode(imageNamed: "Asteroid")
-        enemy.scale(to: CGSize(width: 30, height: 30))
-        enemy.position = createRandomCoordinate()
-        enemy.run(rotateForever())
-        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
-        enemy.physicsBody?.categoryBitMask = Categories.enemy
-        enemy.physicsBody?.contactTestBitMask = Categories.rocket
-        
-        addChild(enemy)
-        moveEnemy(enemy)
-    }
-    
-    func createRandomCoordinate() -> CGPoint {
-        let x: CGFloat
-        let y: CGFloat
-        
-        let topOrBottom = arc4random_uniform(2) == 0
-        
-        if topOrBottom {
-            x = randomXPosition()
-            if arc4random_uniform(2) == 0 {
-                y = 0
-            } else {
-                y = frame.height
-            }
-        } else {
-            y = randomYPosition()
-            if arc4random_uniform(2) == 0 {
-                x = 0
-            } else {
-                x = frame.width
-            }
-        }
-        
-        return CGPoint(x: x, y: y)
-    }
-    
-    //MARK: Animation
-    private func rotateForever() -> SKAction {
-        let oneRevolution = SKAction.rotate(byAngle: CGFloat(-Double.pi * 2), duration: 5.0)
-        let repeated = SKAction.repeatForever(oneRevolution)
-        
-        return repeated
-    }
-    
-    private func fireRocket(_ rocket: SKSpriteNode) {
-        let r: CGFloat = 50
-        
-        let radianFactor:CGFloat = 0.0174532925;
-        let rotationInDegrees = rocket.zRotation / radianFactor;
-        let newRotationDegrees = rotationInDegrees + 90;
-        let newRotationRadians = newRotationDegrees * radianFactor;
-        
-        
-        let dx = r * cos(newRotationRadians)
-        let dy = r * sin(newRotationRadians)
-        // Specify the force to apply to the SKPhysicsBody
-        rocket.physicsBody?.applyImpulse(CGVector(dx: dx, dy: dy))
-    }
-    
-    private func moveEnemy(_ enemy: SKSpriteNode) {
-        let moveToCenter = SKAction.move(to: center, duration: 10)
-        enemy.run(moveToCenter)
-    }
+    let player = SKSpriteNode(imageNamed: "Character")
+    lazy var ground = makeGround()
     
     override func sceneDidLoad() {
         super.sceneDidLoad()
-       
-        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         
-        addChild(deathStar)
-        deathStar.run(rotateForever())
+        ground.fillColor = .green
+        ground.strokeColor = .green
+        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.frame.size, center: CGPoint(x: ground.frame.width / 2, y: ground.frame.height / 2))
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.categoryBitMask = Categories.ground
+        addChild(ground)
+        
+        player.position.x = frame.midX
+        player.position.y = 200
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.collisionBitMask = Categories.ground
+        player.physicsBody?.contactTestBitMask = Categories.coins
+        addChild(player)
+        
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { [weak self] _ in
+            guard let scene = self else {
+                return
+            }
+            
+            let money = SKLabelNode(text: "💰")
+            money.setScale(2)
+            money.verticalAlignmentMode = .center
+            money.position.x = -money.frame.width
+            money.position.y = 300
+            money.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+            money.physicsBody?.isDynamic = false
+            money.physicsBody?.categoryBitMask = Categories.coins
+            scene.addChild(money)
+            
+            money.run(.moveTo(x: scene.size.width + money.frame.width, duration: 3)) {
+                money.removeFromParent()
+            }
+        }
         
         physicsWorld.contactDelegate = self
-        
-        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true, block: { [weak self] _ in
-            self?.makeEnemy()
-            
-        })
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         
-        makeRocket()
+        let playerBody = player.physicsBody!
+        let groundBody = ground.physicsBody!
+        
+        guard playerBody.allContactedBodies().contains(groundBody) else {
+            return
+        }
+        
+        playerBody.applyImpulse(CGVector(dx: 0, dy: 200))
     }
     
-    var center: CGPoint {
-        return CGPoint(x: frame.midX, y: frame.midY)
-    }
-    
-    /// Return a random X position within the scene
-    func randomXPosition() -> CGFloat {
-        return CGFloat(arc4random_uniform(UInt32(size.width)))
-    }
-    
-    /// Return a random Y position iwthin the scene
-    func randomYPosition() -> CGFloat {
-        return CGFloat(arc4random_uniform(UInt32(size.height)))
+    private func makeGround() -> SKShapeNode {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: 100)
+        return SKShapeNode(rect: rect)
     }
 }
 
 extension Scene: SKPhysicsContactDelegate {
-    
     func didBegin(_ contact: SKPhysicsContact) {
-        print(contact)
-        let enemyBody: SKPhysicsBody
-        let rocketBody: SKPhysicsBody
-        
-        if contact.bodyA.categoryBitMask == Categories.enemy {
-            enemyBody = contact.bodyA
-            rocketBody = contact.bodyB
+        if contact.bodyA.node == player {
+            contact.bodyB.node?.removeFromParent()
         } else {
-            enemyBody = contact.bodyB
-            rocketBody = contact.bodyA
-        }
-            
-        let textures: [SKTexture] = [
-            SKTexture(imageNamed: "Explosion-0"),
-            SKTexture(imageNamed: "Explosion-1"),
-            SKTexture(imageNamed: "Explosion-2"),
-            SKTexture(imageNamed: "Explosion-3"),
-            SKTexture(imageNamed: "Explosion-4"),
-            SKTexture(imageNamed: "Explosion-5"),
-            SKTexture(imageNamed: "Explosion-6")
-        ]
-        let enemy = enemyBody.node
-        let rocket = rocketBody.node
-        
-        enemy?.run(.animate(with: textures, timePerFrame: 0.05)) {
-            enemy?.removeFromParent()
-            rocket?.removeFromParent()
+            contact.bodyA.node?.removeFromParent()
         }
     }
 }
