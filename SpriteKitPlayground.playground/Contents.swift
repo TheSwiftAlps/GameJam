03deb5c10b8ae8546fb3a2551e114c1ae3b31d16
John Sundell <john@sundell.co>
2017-11-23 14:32:18 +0100

    Reset playground

--- SpriteKitPlayground.playground/Contents.swift
+++ SpriteKitPlayground.playground/Contents.swift
@@ -3,16 +3,13 @@ import PlaygroundSupport
 
 struct Categories {
     static let ground: UInt32 = 1
-    static let enemy: UInt32 = 1 << 1
+    static let coins: UInt32 = 1 << 1
 }
 
 class Scene: SKScene {
     let player = SKSpriteNode(imageNamed: "Character")
-    let fire = SKSpriteNode(imageNamed: "Explosion-0")
     lazy var ground = makeGround()
-    var score = 0
-    let scoreLabel = SKLabelNode(text: "Score:")
-    
+
     override func sceneDidLoad() {
         super.sceneDidLoad()
 
@@ -27,101 +24,59 @@ class Scene: SKScene {
         player.position.y = 200
         player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
         player.physicsBody?.collisionBitMask = Categories.ground
+        player.physicsBody?.contactTestBitMask = Categories.coins
         addChild(player)
-        
-        createFire()
-        
-        scoreLabel.position.x = 100
-        scoreLabel.position.y = 600
-        scoreLabel.text = "Score: \(score)"
-        addChild(scoreLabel)
-
 
         Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { [weak self] _ in
             guard let scene = self else {
                 return
             }
 
-            let enemy = SKSpriteNode(imageNamed: "Character")
-            enemy.position.x = -enemy.frame.width
-            enemy.position.y = 300
-            enemy.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-            enemy.physicsBody?.isDynamic = false
-            enemy.physicsBody?.categoryBitMask = Categories.enemy
-            scene.addChild(enemy)
+            let money = SKLabelNode(text: "💰")
+            money.setScale(2)
+            money.verticalAlignmentMode = .center
+            money.position.x = -money.frame.width
+            money.position.y = 300
+            money.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+            money.physicsBody?.isDynamic = false
+            money.physicsBody?.categoryBitMask = Categories.coins
+            scene.addChild(money)
 
-            enemy.run(.moveTo(x: scene.size.width + enemy.frame.width, duration: 3)) {
-                enemy.removeFromParent()
+            money.run(.moveTo(x: scene.size.width + money.frame.width, duration: 3)) {
+                money.removeFromParent()
             }
         }
 
         physicsWorld.contactDelegate = self
-
-        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        
-        fire.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 200))
+
+        let playerBody = player.physicsBody!
+        let groundBody = ground.physicsBody!
+
+        guard playerBody.allContactedBodies().contains(groundBody) else {
+            return
+        }
+
+        playerBody.applyImpulse(CGVector(dx: 0, dy: 200))
     }
 
     private func makeGround() -> SKShapeNode {
         let rect = CGRect(x: 0, y: 0, width: size.width, height: 100)
         return SKShapeNode(rect: rect)
     }
-    
-    func createFire() {
-        fire.position.x = frame.midX
-        fire.position.y = 220
-        fire.physicsBody = SKPhysicsBody(rectangleOf: fire.size)
-        fire.physicsBody?.contactTestBitMask = Categories.enemy
-        fire.physicsBody?.collisionBitMask = Categories.ground
-        addChild(fire)
-    }
-
 }
 
 extension Scene: SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node == fire {
-            if contact.bodyB.node != player {
+        if contact.bodyA.node == player {
             contact.bodyB.node?.removeFromParent()
-                updateScore()
-                explode()
-            }
         } else {
-            if contact.bodyA.node != player {
-                contact.bodyA.node?.removeFromParent()
-                updateScore()
-                explode()
-            }
+            contact.bodyA.node?.removeFromParent()
         }
     }
-    
-    func updateScore() {
-        score += 1
-        scoreLabel.text = "Score: \(score)"
-    }
-    
-    func explode() {
-        let textures: [SKTexture] = [
-            SKTexture(imageNamed: "Explosion-0"),
-            SKTexture(imageNamed: "Explosion-1"),
-            SKTexture(imageNamed: "Explosion-2"),
-            SKTexture(imageNamed: "Explosion-3"),
-            SKTexture(imageNamed: "Explosion-4"),
-            SKTexture(imageNamed: "Explosion-5"),
-            SKTexture(imageNamed: "Explosion-6"),
-            SKTexture(imageNamed: "Explosion-0")
-        ]
-        fire.run(.animate(with: textures, timePerFrame: 0.05)) {
-            self.fire.removeFromParent()
-            
-        }
-        self.fire.physicsBody = nil
-    }
-
 }
 
 let viewFrame = CGRect(x: 0, y: 0, width: 365, height: 667)
